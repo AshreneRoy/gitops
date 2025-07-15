940aa63f5540719357a0794c94706828cffca9d8
Ashrene Roy <ashrene_roy@mckinsey.com>
2025-07-15 21:55:27 +0530

    feat: add monitoring

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
 @app.get("/")
 async def main():
+    print(f'Logging from {__name__}')
     return {"message": "Hello World"}
+
+Instrumentator().instrument(app).expose(app)
